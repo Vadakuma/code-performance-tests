fe1016bbbefb30821cf7b49d5c6faf6b89188f88
Vadim Bezrukov <vadakuma@gmail.com>
2019-10-21 23:34:26 +0300

    - added base for string and containskey tests

---
+++ Assets/Scripts/Tests/ContainsKeyTest.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+namespace TestPerformance
+{
+    public class ContainsKeyTest : Test
+    {
+        public int Counter = 1000;
+        public int DictionaryCounter = 3000;
+
+        private Dictionary<string, string>  _dict = new Dictionary<string, string>();
+        private string                      _keyrandom;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            for (int idx = 0; idx < DictionaryCounter; ++idx)
+                _dict.Add(System.Guid.NewGuid().ToString(), System.Guid.NewGuid().ToString());
+        }
+
+        public override void DoTest()
+        {
+            base.DoTest();
+            //_keyrandom = Random.Range(_dict.Count/2, _dict.Count - 1);
+            _keyrandom = System.Guid.NewGuid().ToString();
+            // avoiding memory allocate procces impact time
+            TestContainsKey(1);
+
+            var resultContainsKey = TestContainsKey(Attempts);
+
+            var resulttrygetvalue = TestTryGetValue(Attempts);
+
+            WriteResult(Counter.ToString(), resultContainsKey, resulttrygetvalue);
+        }
+
+
+        private string TestContainsKey(int attempts)
+        {
+            timeResult = 0;
+
+            for (int idx = 0; idx < attempts; ++idx)
+            {
+                Stopwatch stopwatch = new Stopwatch(); stopwatch.Start();
+
+                for (int jdx = 0; jdx < Counter; ++jdx)
+                {
+                    if (_dict.ContainsKey(_keyrandom))
+                    {
+                        var value = _dict[_keyrandom];
+                    }
+                }
+
+                // Stop timing.
+                stopwatch.Stop();
+                timeResult += stopwatch.Elapsed.TotalSeconds;
+            }
+            timeResult /= attempts;
+
+            return "Dictionary ContainsKey test.  Time elapsed:  " + timeResult.ToString() + " in seconds";
+        }
+
+        private string TestTryGetValue(int attempts)
+        {
+            timeResult = 0;
+
+            for (int idx = 0; idx < attempts; ++idx)
+            {
+                Stopwatch stopwatch = new Stopwatch(); stopwatch.Start();
+
+                for (int jdx = 0; jdx < Counter; ++jdx)
+                {
+                    if(_dict.TryGetValue(_keyrandom, out string value))
+                    {
+                        //do something with value
+                    }
+                }
+
+                // Stop timing.
+                stopwatch.Stop();
+                timeResult += stopwatch.Elapsed.TotalSeconds;
+            }
+            timeResult /= attempts;
+
+            return "Dictionary TryGetValue test.  Time elapsed:  " + timeResult.ToString() + " in seconds";
+        }
+    }
+}
---
+++ Assets/Scripts/Tests/StringTests.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+namespace TestPerformance
+{
+    public class StringTests : Test
+    {
+        public int Counter = 1000;
+
+
+        public override void DoTest()
+        {
+            base.DoTest();
+            // avoiding memory allocate procces impact time
+            DoStringWithBoxing(1);
+
+            var resultboxing = DoStringWithBoxing(Attempts);
+
+            var resultwithout = DoStringWithOutBoxing(Attempts);
+
+            WriteResult(Counter.ToString(), resultboxing, resultwithout);
+        }
+
+        private string DoStringWithBoxing(int attempts)
+        {
+            timeResult = 0;
+
+            for (int idx = 0; idx < attempts; ++idx)
+            {
+                Stopwatch stopwatch = new Stopwatch(); stopwatch.Start();
+
+                for (int jdx = 0; jdx < Counter; ++jdx)
+                {
+                    var str = jdx + "|" + jdx + "|" + jdx;
+                }
+
+                // Stop timing.
+                stopwatch.Stop();
+                timeResult += stopwatch.Elapsed.TotalSeconds;
+            }
+            timeResult /= attempts;
+
+            return "String boxing test.  Time elapsed:  " + timeResult.ToString() + " in seconds";
+        }
+
+        private string DoStringWithOutBoxing(int attempts)
+        {
+            timeResult = 0;
+
+            for (int idx = 0; idx < attempts; ++idx)
+            {
+                Stopwatch stopwatch = new Stopwatch(); stopwatch.Start();
+
+                for (int jdx = 0; jdx < Counter; ++jdx)
+                {
+                    var str = jdx.ToString() + "|" + jdx.ToString() + "|" + jdx.ToString();
+                }
+
+                // Stop timing.
+                stopwatch.Stop();
+                timeResult += stopwatch.Elapsed.TotalSeconds;
+            }
+            timeResult /= attempts;
+
+            return "String with int.ToString() test.  Time elapsed:  " + timeResult.ToString() + " in seconds";
+        }
+    }
+}
